407bde1ef780f8159d54508b2430428639d66469
Nadira132005 <nadira.bodrogean@gmail.com>
2025-05-25 18:04:36 +0300

    fix: update age only for valid candidates in LRU logic

--- cache_controller.v
+++ cache_controller.v
@@ -276,10 +276,22 @@ module cache_controller #(
                       hit_3 ? candidate_3_age :
                       hit_4 ? candidate_4_age : 2'b00;
 
-  assign age_1 = hit_1 ? 2'b00 : candidate_1_age < LRU_prev_age ? candidate_1_age + 1 : candidate_1_age;
-  assign age_2 = hit_2 ? 2'b00 : candidate_2_age < LRU_prev_age ? candidate_2_age + 1 : candidate_2_age;
-  assign age_3 = hit_3 ? 2'b00 : candidate_3_age < LRU_prev_age ? candidate_3_age + 1 : candidate_3_age;
-  assign age_4 = hit_4 ? 2'b00 : candidate_4_age < LRU_prev_age ? candidate_4_age + 1 : candidate_4_age;
+  assign age_1 = hit_1 ? 2'b00 : 
+                 (candidate_1_valid) ? 
+                 (candidate_1_age < LRU_prev_age ? candidate_1_age + 1 : candidate_1_age) : 
+                 candidate_1_age;
+  assign age_2 = hit_2 ? 2'b00 : 
+                 (candidate_2_valid) ? 
+                 (candidate_2_age < LRU_prev_age ? candidate_2_age + 1 : candidate_2_age) : 
+                 candidate_2_age;
+  assign age_3 = hit_3 ? 2'b00 : 
+                 (candidate_3_valid) ? 
+                 (candidate_3_age < LRU_prev_age ? candidate_3_age + 1 : candidate_3_age) : 
+                 candidate_3_age;
+  assign age_4 = hit_4 ? 2'b00 : 
+                 (candidate_4_valid) ? 
+                 (candidate_4_age < LRU_prev_age ? candidate_4_age + 1 : candidate_4_age) : 
+                 candidate_4_age;
 
   assign candidate_write[DIRTY_BIT_START+DIRTY_BIT-1:DIRTY_BIT_START] =
       // Set dirty on WRITE (either hit or miss)
